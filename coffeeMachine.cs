72d2356108522ef4bff515e573067c60d41954f6
Peter Bozov <peter.bozov@gmail.com>
2019-07-01 14:27:08 +0300

    Coffee vending machine console app. Needs finishing.

---
+++ coffeeMachine.cs
@@ -0,0 +1,39 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int coinsFive = int.Parse(Console.ReadLine());
+        int coinsTen = int.Parse(Console.ReadLine());
+        int coinsTwenty = int.Parse(Console.ReadLine());
+        int coinsFifty = int.Parse(Console.ReadLine());
+        int coinsLev = int.Parse(Console.ReadLine());
+        double moneyGiven = double.Parse(Console.ReadLine());
+        double drinkPrice = double.Parse(Console.ReadLine());
+
+        if (moneyGiven < drinkPrice)
+        {
+            Console.WriteLine("More {0: 0.00}", drinkPrice - moneyGiven);
+            return;
+        }
+
+        if (moneyGiven == drinkPrice)
+        {
+            Console.WriteLine("Yes 0.00");
+            return;
+        }
+
+        double difference = moneyGiven - drinkPrice;
+        double totalOfFive = coinsFive * 0.05;
+        double totalOfTen = coinsTen * 0.10;
+        double totalOfTwenty = coinsTwenty * 0.20;
+        double totalOfFifty = coinsFifty * 0.50;
+        double totalOfLev = coinsLev * 1.00;
+        double sumOfMoney = totalOfFive + totalOfTen + totalOfTwenty + totalOfFifty + totalOfLev;
+
+        while (difference > 0)
+        {
+
+        }
+    }
+}
